de8098cdc0e13ca8164b93486a8370bd55f0b013
NovaeChan <novaestar@proton.me>
2024-04-29 20:15:38 +0200

    Modified apiCall

--- src/api/apiCall.js
+++ src/api/apiCall.js
@@ -1,14 +1,14 @@
 import datas from './denominations-emprises-voies-actuelles.json'
 
 export default function FetchData() {
-    for(let [i, data] of datas.entries()){
-        if(data.typo.includes("VOIE ")){
-            datas.splice(i, 1);
-        }
-        // if(data.typo.includes("PLACE")){
-        //     datas.splice(i, 1);
-        // }
-    }
+    // for(let [i, data] of datas.entries()){
+    //     if(data.typo.includes("VOIE ")){
+    //         datas.splice(i, 1);
+    //     }
+    //     // if(data.typo.includes("PLACE")){
+    //     //     datas.splice(i, 1);
+    //     // }
+    // }
     return datas;    
 }
 
